cce598b7a9d909371323ff639babef3702ef5938
Pavlo Phanel <devpavlophanel@gmail.com>
2025-01-11 10:48:16 +0100

    added buttons

--- script.js
+++ script.js
@@ -1,6 +1,197 @@
-var num1 = 11;
-var num2 = 11;
-var operator = "-";
+var container = document.querySelector("#container");
+var inputContainer = document.createElement("div");
+inputContainer.classList.add("inputContainer");
+container.appendChild(inputContainer);
+var input = document.createElement("div");
+var inputContent;
+console.log(inputContent);
+input.classList.add("input");
+inputContainer.appendChild(input);
+var buttonsFieldContainer = document.createElement("div");
+buttonsFieldContainer.classList.add("buttonsFieldContainer");
+container.appendChild(buttonsFieldContainer);
+var buttonNum1 = document.createElement("button");
+buttonNum1.textContent = "1";
+buttonNum1.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum1);
+function sendOne() {
+    if (inputContent === undefined) {
+        inputContent = "1";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "1";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum1.addEventListener("click", sendOne);
+var buttonNum2 = document.createElement("button");
+buttonNum2.textContent = "2";
+buttonNum2.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum2);
+function sendTwo() {
+    if (inputContent === undefined) {
+        inputContent = "2";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "2";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum2.addEventListener("click", sendTwo);
+var buttonNum3 = document.createElement("button");
+buttonNum3.textContent = "3";
+buttonNum3.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum3);
+function sendThree() {
+    if (inputContent === undefined) {
+        inputContent = "3";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "3";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum3.addEventListener("click", sendThree);
+var buttonNum4 = document.createElement("button");
+buttonNum4.textContent = "4";
+buttonNum4.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum4);
+function sendFour() {
+    if (inputContent === undefined) {
+        inputContent = "4";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "4";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum4.addEventListener("click", sendFour);
+var buttonNum5 = document.createElement("button");
+buttonNum5.textContent = "5";
+buttonNum5.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum5);
+function sendFive() {
+    if (inputContent === undefined) {
+        inputContent = "5";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "5";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum5.addEventListener("click", sendFive);
+var buttonNum6 = document.createElement("button");
+buttonNum6.textContent = "6";
+buttonNum6.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum6);
+function sendSix() {
+    if (inputContent === undefined) {
+        inputContent = "6";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "6";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum6.addEventListener("click", sendSix);
+var buttonNum7 = document.createElement("button");
+buttonNum7.textContent = "7";
+buttonNum7.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum7);
+function sendSeven() {
+    if (inputContent === undefined) {
+        inputContent = "7";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "7";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum7.addEventListener("click", sendSeven);
+var buttonNum8 = document.createElement("button");
+buttonNum8.textContent = "8";
+buttonNum8.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum8);
+function sendEight() {
+    if (inputContent === undefined) {
+        inputContent = "8";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "8";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum8.addEventListener("click", sendEight);
+var buttonNum9 = document.createElement("button");
+buttonNum9.textContent = "9";
+buttonNum9.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum9);
+function sendNine() {
+    if (inputContent === undefined) {
+        inputContent = "9";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "9";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum9.addEventListener("click", sendNine);
+var buttonNum0 = document.createElement("button");
+buttonNum0.textContent = "0";
+buttonNum0.classList.add("button");
+buttonsFieldContainer.appendChild(buttonNum0);
+function sendZero() {
+    if (inputContent === undefined) {
+        inputContent = "0";
+        return input.textContent = inputContent;
+    }
+    else {
+        inputContent = inputContent + "0";
+        return input.textContent = inputContent;
+    }
+}
+buttonNum0.addEventListener("click", sendZero);
+var buttonEqual = document.createElement("button");
+buttonEqual.textContent = "=";
+buttonEqual.classList.add("button");
+buttonsFieldContainer.appendChild(buttonEqual);
+var buttonClear = document.createElement("button");
+buttonClear.textContent = "C";
+buttonClear.classList.add("button");
+buttonsFieldContainer.appendChild(buttonClear);
+function clear() {
+    inputContent = "";
+    return input.textContent = inputContent;
+}
+buttonClear.addEventListener("click", clear);
+var buttonAdd = document.createElement("button");
+buttonAdd.textContent = "+";
+buttonAdd.classList.add("button");
+buttonsFieldContainer.appendChild(buttonAdd);
+var buttonSubtract = document.createElement("button");
+buttonSubtract.textContent = "-";
+buttonSubtract.classList.add("button");
+buttonsFieldContainer.appendChild(buttonSubtract);
+var buttonMultiply = document.createElement("button");
+buttonMultiply.textContent = "x";
+buttonMultiply.classList.add("button");
+buttonsFieldContainer.appendChild(buttonMultiply);
+var buttonDivide = document.createElement("button");
+buttonDivide.textContent = "/";
+buttonDivide.classList.add("button");
+buttonsFieldContainer.appendChild(buttonDivide);
+var num1;
+var num2;
+var operator;
 function add(num1, num2) {
     var sum = num1 + num2;
     return sum;
@@ -31,6 +222,6 @@ function operate() {
     else {
         result = divide(num1, num2);
     }
-    console.log(result);
+    ;
 }
 operate();
